e5053c9bf3887af544b376386649818cdc6adb9c
zyckoss <monkeyleapo4@gmail.com>
2025-02-10 14:56:11 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function moveElmRand(elm) {
+  elm.style.position = "absolute";
+  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+}
+
+const moveRandom = document.querySelector("#move-random");
+
+moveRandom.addEventListener("mouseenter", function (e) {
+  moveElmRand(e.target);
+});
